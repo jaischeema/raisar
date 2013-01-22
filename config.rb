6038e8f452aa25ff5bbb4bcf6ca43d0cfa675efa
Jais Cheema <jaischeema@gmail.com>
2013-01-23 10:52:45 +1100

    Add basic components for design - compass

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :expanded #or :nested or :compact or :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
